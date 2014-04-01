939c0d7b66b8142d9b549df3eb6026dca11123b8
John Brunton <jbrunton@zipcar.com>
2014-04-01 16:47:07 +0100

    fixed missing trailing whitespace in tests

--- spec/bankocr_spec.rb
+++ spec/bankocr_spec.rb
@@ -25,7 +25,7 @@ describe BankOCR do
 
   it 'should recognize zero' do
     input = <<-END
- _  _  _  _  _  _  _  _  _
+ _  _  _  _  _  _  _  _  _ 
 | || || || || || || || || |
 |_||_||_||_||_||_||_||_||_|
 
@@ -36,7 +36,7 @@ describe BankOCR do
 
   it 'should recognize one' do
     input = <<-END
- _  _  _  _  _  _  _  _
+ _  _  _  _  _  _  _  _    
 | || || || || || || || |  |
 |_||_||_||_||_||_||_||_|  |
 
@@ -47,7 +47,7 @@ describe BankOCR do
 
   it 'should recognize ten' do
     input = <<-END
- _  _  _  _  _  _  _     _
+ _  _  _  _  _  _  _     _ 
 | || || || || || || |  || |
 |_||_||_||_||_||_||_|  ||_|
 
@@ -58,7 +58,7 @@ describe BankOCR do
 
   it 'should recognise eleven' do
     input = <<-END
- _  _  _  _  _  _  _
+ _  _  _  _  _  _  _       
 | || || || || || || |  |  |
 |_||_||_||_||_||_||_|  |  |
 
@@ -69,7 +69,7 @@ describe BankOCR do
 
   it 'should recognise one hundred' do
     input = <<-END
- _  _  _  _  _  _     _  _
+ _  _  _  _  _  _     _  _ 
 | || || || || || |  || || |
 |_||_||_||_||_||_|  ||_||_|
 
@@ -80,7 +80,7 @@ describe BankOCR do
 
   it 'should recognise one hundred and one' do
     input = <<-END
- _  _  _  _  _  _     _
+ _  _  _  _  _  _     _    
 | || || || || || |  || |  |
 |_||_||_||_||_||_|  ||_|  |
 
@@ -91,7 +91,7 @@ describe BankOCR do
 
   it 'should recognise the highest value digit' do
     input = <<-END
-    _  _     _  _     _
+    _  _     _  _     _    
   || || |  || || |  || |  |
   ||_||_|  ||_||_|  ||_|  |
 
@@ -99,4 +99,5 @@ describe BankOCR do
 
     expect(read(input)).to eq(100100101)
   end
+
 end
