f3332c223f29c13deae7297a02ef22e557a5441b
John Brunton <jbrunton@zipcar.com>
2014-04-01 16:10:24 +0100

    added 101 test case and refactored with read_digit method

--- spec/bankocr_spec.rb
+++ spec/bankocr_spec.rb
@@ -4,17 +4,17 @@ DIGIT_WIDTH = 3
 LAST_MIDDLE_CHAR = 25
 
 def read(input)
-  if input[LAST_MIDDLE_CHAR - DIGIT_WIDTH * 2] == '_'
-    if input[LAST_MIDDLE_CHAR - DIGIT_WIDTH] == '_'
-      return read_last_digit(input)
+  if read_digit(input, 2) == 0
+    if read_digit(input, 1) == 0
+      return read_digit(input, 0)
     end
-    return 10 + read_last_digit(input)
+    return 10 + read_digit(input, 0)
   end
-  100
+  100 + read_digit(input, 0)
 end
 
-def read_last_digit(input)
-  if input[LAST_MIDDLE_CHAR] == '_'
+def read_digit(input, position)
+  if input[LAST_MIDDLE_CHAR - DIGIT_WIDTH * position] == '_'
     0
   else
     1
@@ -76,5 +76,16 @@ describe BankOCR do
     END
 
     expect(read(input)).to eq(100)
+  end
+
+  it 'should recognise one hundred and one' do
+    input = <<-END
+ _  _  _  _  _  _     _
+| || || || || || |  || |  |
+|_||_||_||_||_||_|  ||_|  |
+
+    END
+
+    expect(read(input)).to eq(101)
   end
 end
