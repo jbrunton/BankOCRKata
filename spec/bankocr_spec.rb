d9a82b3ea822d5535c4b13cc06fa24f18941f177
John Brunton <jbrunton@zipcar.com>
2014-04-02 11:48:49 +0100

    implemented checksum method

--- spec/bankocr_spec.rb
+++ spec/bankocr_spec.rb
@@ -41,181 +41,222 @@ def read_char(input, position, line, offset)
   return input[LINE_LENGTH * line + DIGIT_WIDTH * (NUMBER_OF_DIGITS - position - 1) + offset]
 end
 
-describe BankOCR do
+def checksum(account_number)
+  (0..NUMBER_OF_DIGITS).inject(0) do |sum, digit|
+    sum + get_checksum_component(account_number, digit)
+  end % 11
+end
 
-  it 'should recognize zero' do
-    input = <<-END
+def get_checksum_component(number, digit)
+  get_decimal_digit(number, digit) * (digit + 1)
+end
+
+def get_decimal_digit(number, digit)
+  number % 10**(digit + 1) / 10**digit
+end
+
+describe BankOCR do
+  context 'reading numbers' do
+    it 'should recognize zero' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || || |
 |_||_||_||_||_||_||_||_||_|
 
-    END
-
-    expect(read(input)).to eq(0)
-  end
-
-  it 'should recognize one' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(0)
+    end
+  
+    it 'should recognize one' do
+      input = <<-END
  _  _  _  _  _  _  _  _    
 | || || || || || || || |  |
 |_||_||_||_||_||_||_||_|  |
 
-    END
-
-    expect(read(input)).to eq(1)
-  end
-
-  it 'should recognize ten' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(1)
+    end
+  
+    it 'should recognize ten' do
+      input = <<-END
  _  _  _  _  _  _  _     _ 
 | || || || || || || |  || |
 |_||_||_||_||_||_||_|  ||_|
 
-    END
-
-    expect(read(input)).to eq(10)
-  end
-
-  it 'should recognise eleven' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(10)
+    end
+  
+    it 'should recognise eleven' do
+      input = <<-END
  _  _  _  _  _  _  _       
 | || || || || || || |  |  |
 |_||_||_||_||_||_||_|  |  |
 
-    END
-
-    expect(read(input)).to eq(11)
-  end
-
-  it 'should recognise one hundred' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(11)
+    end
+  
+    it 'should recognise one hundred' do
+      input = <<-END
  _  _  _  _  _  _     _  _ 
 | || || || || || |  || || |
 |_||_||_||_||_||_|  ||_||_|
 
-    END
-
-    expect(read(input)).to eq(100)
-  end
-
-  it 'should recognise one hundred and one' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(100)
+    end
+  
+    it 'should recognise one hundred and one' do
+      input = <<-END
  _  _  _  _  _  _     _    
 | || || || || || |  || |  |
 |_||_||_||_||_||_|  ||_|  |
 
-    END
-
-    expect(read(input)).to eq(101)
-  end
-
-  it 'should recognise the highest value digit' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(101)
+    end
+  
+    it 'should recognise the highest value digit' do
+      input = <<-END
     _  _     _  _     _    
   || || |  || || |  || |  |
   ||_||_|  ||_||_|  ||_|  |
 
-    END
-
-    expect(read(input)).to eq(100100101)
-  end
-
-  it 'should recognize two' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(100100101)
+    end
+  
+    it 'should recognize two' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || | _|
 |_||_||_||_||_||_||_||_||_ 
 
-    END
-
-    expect(read(input)).to eq(2)
-  end
-
-  it 'should recognize three' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(2)
+    end
+  
+    it 'should recognize three' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || | _|
 |_||_||_||_||_||_||_||_| _|
 
-    END
-
-    expect(read(input)).to eq(3)
-  end
-
-  it 'should recognize four' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(3)
+    end
+  
+    it 'should recognize four' do
+      input = <<-END
  _  _  _  _  _  _  _  _    
 | || || || || || || || ||_|
 |_||_||_||_||_||_||_||_|  |
 
-    END
-
-    expect(read(input)).to eq(4)
-  end
-
-  it 'should recognize five' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(4)
+    end
+  
+    it 'should recognize five' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || ||_ 
 |_||_||_||_||_||_||_||_| _|
 
-    END
-
-    expect(read(input)).to eq(5)
-  end
-
-  it 'should recognize six' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(5)
+    end
+  
+    it 'should recognize six' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || ||_ 
 |_||_||_||_||_||_||_||_||_|
 
-    END
-
-    expect(read(input)).to eq(6)
-  end
-
-  it 'should recognize seven' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(6)
+    end
+  
+    it 'should recognize seven' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || |  |
 |_||_||_||_||_||_||_||_|  |
 
-    END
-
-    expect(read(input)).to eq(7)
-  end
-
-  it 'should recognize eight' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(7)
+    end
+  
+    it 'should recognize eight' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || ||_|
 |_||_||_||_||_||_||_||_||_|
 
-    END
-
-    expect(read(input)).to eq(8)
-  end
-
-  it 'should recognize nine' do
-    input = <<-END
+      END
+  
+      expect(read(input)).to eq(8)
+    end
+  
+    it 'should recognize nine' do
+      input = <<-END
  _  _  _  _  _  _  _  _  _ 
 | || || || || || || || ||_|
 |_||_||_||_||_||_||_||_| _|
 
-    END
-
-    expect(read(input)).to eq(9)
-  end
+      END
   
-  it 'should recognize any digit in any position' do
-    input = <<-END
+      expect(read(input)).to eq(9)
+    end
+    
+    it 'should recognize any digit in any position' do
+      input = <<-END
     _  _     _  _  _  _  _ 
   | _| _||_||_ |_   ||_||_|
   ||_  _|  | _||_|  ||_| _|
                            
-    END
+      END
+      
+      expect(read(input)).to eq(123456789)
+    end
+  end
+  
+  context 'checksums' do
+    (0..9).each do |account_number|
+      it "should give #{account_number} for #{account_number}" do
+        expect(checksum(account_number)).to eq(account_number)
+      end
+    end
     
-    expect(read(input)).to eq(123456789)
+    {
+        10 => 2,
+        11 => 3,
+        19 => 0,
+        20 => 4,
+        60 => 1,
+        100 => 3,
+        101 => 4,
+        111 => 6,
+        221 => 0,
+        123456789 => 0,
+        987654321 => 10
+    }.each_pair do |account_number, expected_checksum|
+      it "should give #{expected_checksum} for the account number #{account_number}" do
+        expect(checksum(account_number)).to eq(expected_checksum)
+      end
+    end
   end
 end
